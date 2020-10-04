e6ccc21d64a333f0de0bb879b8d2946970bcc14b
Atte <atte.perttola@gmail.com>
2020-10-04 20:49:40 +0300

    progress bar initial

--- src/index.js
+++ src/index.js
@@ -55,7 +55,19 @@ function renderBoard() {
   }
 }
 
-function progressBar() {}
+function progressBar() {
+  let elem = document.getElementById("progressBar");
+  let width = 1;
+  let id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + "%";
+    }
+  }
+}
 
 function checkWinner(cell) {
   let paske = cell.getAttribute("id");
